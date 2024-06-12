a8211698c5c78480b09576e5a188d43ba23b5cc2
Soumik Roy Chowdhury <eventssoumik01@gmail.com>
2024-06-12 16:21:27 +0530

    Complete Query Solver App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,34 @@
-import {useState} from 'react';
+import {useState,useRef} from 'react';
+import DOMPurify from 'dompurify';
+import {marked} from 'marked';
+marked.use({gfm:true})
 function App() {
-  const [data,setData]=useState('');
+  const [data,setData]=useState([]);
+  const [query,setQuery]=useState('');
+  const inputRef=useRef(null);
+  function handleKeyDown(e){
+    if(e.key==='Enter'&&!e.shiftKey){
+      e.preventDefault();
+      getAnswer();
+      inputRef.current.blur();
+    }
+  }
   function getAnswer(){
-    const query="Write a story about software engineering and software development."
-    fetch('/api',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({'query':query})}).then(res=>res.json()).then(data=>{setData(data);console.log(data);})
+    setData('');
+    if(query!==''){
+      fetch('/api',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({'query':query})}).then(res=>res.json()).then(data=>{const val=DOMPurify.sanitize(marked(data)); setData(val);inputRef.current.focus();setQuery('');console.log(data);}).catch(err=>console.log(err));
+    }
   }
   return (
     <div style={{height:'100vh',display:'flex',flexDirection:'column'}}>
-      <h1 style={{padding:'10px',marginBottom:'0'}}>Ask Your Queries Here</h1>
-      <div style={{margin:'0',flexGrow:'1'}}>
+      <h1 style={{padding:'10px',marginBottom:'0',textAlign:'center',color:'lightblue'}}>Ask Your Queries Here</h1>
+      <div style={{margin:'0',flexGrow:'1',overflow:'auto'}}>
         <div style={{width:'100%',height:'100%'}}>
-          <article style={{margin:'0'}}>{data}</article>
+          {data===''?(<div style={{textAlign:'center'}}>Let me find some answer to your query.</div>):<article style={{margin:'0'}} dangerouslySetInnerHTML={{__html:data}}/>}
         </div>
       </div>
       <div style={{display:'flex',alignItems:'center',backgroundColor:'#222',padding:'10px'}}>
-        <textarea style={{margin:'0',flexGrow:'1',overflowY:'auto'}} placeholder="Enter Your Query Here"/>
+        <textarea onChange={(e)=>setQuery(e.target.value)} style={{margin:'0',flexGrow:'1',overflowY:'auto'}} placeholder="Enter Your Query Here" onKeyDown={handleKeyDown} ref={inputRef} value={query}/>
         <button onClick={getAnswer} style={{margin:'0',flex:'1',marginLeft:'10px'}}>Get Answer</button>
       </div>
     </div>
