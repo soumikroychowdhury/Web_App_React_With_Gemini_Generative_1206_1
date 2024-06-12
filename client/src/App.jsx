6503ede4a906ff2ec54a293e48430ad14d6188c5
Soumik Roy Chowdhury <eventssoumik01@gmail.com>
2024-06-12 15:04:44 +0530

    API Implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
+import {useState} from 'react';
 function App() {
+  const [data,setData]=useState('');
+  function getAnswer(){
+    const query="Write a story about software engineering and software development."
+    fetch('/api',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({'query':query})}).then(res=>res.json()).then(data=>{setData(data);console.log(data);})
+  }
   return (
     <div style={{height:'100vh',display:'flex',flexDirection:'column'}}>
       <h1 style={{padding:'10px',marginBottom:'0'}}>Ask Your Queries Here</h1>
       <div style={{margin:'0',flexGrow:'1'}}>
         <div style={{width:'100%',height:'100%'}}>
-          <article style={{margin:'0'}}/>
+          <article style={{margin:'0'}}>{data}</article>
         </div>
       </div>
       <div style={{display:'flex',alignItems:'center',backgroundColor:'#222',padding:'10px'}}>
         <textarea style={{margin:'0',flexGrow:'1',overflowY:'auto'}} placeholder="Enter Your Query Here"/>
-        <button style={{margin:'0',flex:'1',marginLeft:'10px'}}>Get Answer</button>
+        <button onClick={getAnswer} style={{margin:'0',flex:'1',marginLeft:'10px'}}>Get Answer</button>
       </div>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const express=require('express');
 const app=express();
 let answer=null;
+const bodyParser=require('body-parser');
+app.use(bodyParser.json());
 require('dotenv').config();
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-app.get('/api',(req,res)=>{
+app.post('/api',(req,res)=>{
+  let query=req.body.query;
   async function run() {
-    const prompt = "Write a story about software engineering and software development."
-    const result = await model.generateContent(prompt);
+    const result = await model.generateContent(query);
     const response = await result.response;
     answer=response.text();
     res.json(answer);
@@ -18,11 +20,3 @@ app.get('/api',(req,res)=>{
 app.listen(3000,()=>{
   console.log('Server is running on port 3000');
 });
-// async function run() {
-//   const prompt = "Write a story about software engineering and software development."
-//   const result = await model.generateContent(prompt);
-//   const response = await result.response;
-//   const text = response.text();
-//   console.log(text);
-// }
-// run();
